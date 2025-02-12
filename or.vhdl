a9ed0727b4a04ceb43bcd777b135c705f55e4f1f
hayden freeman <45716506+haydfree@users.noreply.github.com>
2025-02-12 07:56:29 -0600

    Update or.vhdl

--- or.vhdl
+++ or.vhdl
@@ -1,19 +1,18 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-
 -- define structure of entity
 entity orGate is 
-	-- declare inputs and outputs for entity
-	port (
+    -- declare inputs and outputs for entity
+    port (
     	input1: in std_logic;
         input2: in std_logic;
         output: out std_logic
-        );
+    );
 end orGate;
 
 -- define behavior of entity
 architecture orGateA of orGate is
 begin
-	output <= input1 or input2;
+    output <= input1 or input2;
 end orGateA;
