bb0933d4ce76cf08527f6b78db1e43ffcf2f663c
Hayden Freeman <45716506+haydfree@users.noreply.github.com>
2025-02-13 14:05:38 -0600

    Update chip_xor.vhdl

--- chip_xor.vhdl
+++ chip_xor.vhdl
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity CHIP_XOR is
 
-	port(
+    port(
     	a: in std_logic;
         b: in std_logic;
         q: out std_logic;
@@ -11,12 +11,28 @@ entity CHIP_XOR is
 
 end entity;
 
+-- Nand gate:
+-- | a | b | q |
+-- -------------
+-- | 0 | 0 | 1 |
+-- | 0 | 1 | 1 |
+-- | 1 | 0 | 1 |
+-- | 1 | 1 | 0 |
+
+-- Xor gate:
+-- | a | b | q |
+-- -------------
+-- | 0 | 0 | 0 |
+-- | 0 | 1 | 1 |
+-- | 1 | 0 | 1 |
+-- | 1 | 1 | 0 |
+
 architecture rlt of CHIP_XOR is
 
 begin
-	process(a,b) is
+    process(a,b) is
     begin
-		q <= a xor b;
+        q <= (a nand (a nand b)) nand (b nand (a nand b));
     end process;
 
 end architecture;
