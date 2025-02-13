8ad191a382e5ee07b80d367fa65f0a499cd54605
Hayden Freeman <45716506+haydfree@users.noreply.github.com>
2025-02-13 12:36:39 -0600

    Update chip_not.vhdl

--- chip_not.vhdl
+++ chip_not.vhdl
@@ -3,19 +3,27 @@ use IEEE.std_logic_1164.all;
 
 entity CHIP_NOT is
 
-	port(
+    port(
     	a: in std_logic;
         q: out std_logic;
     );
 
 end entity;
 
+-- Nand gate:
+-- | a | b | q |
+-- -------------
+-- | 0 | 0 | 1 | <-- a nand a = not a
+-- | 0 | 1 | 1 |
+-- | 1 | 0 | 1 |
+-- | 1 | 1 | 0 | <-- a nand a = not a
+
 architecture rlt of CHIP_NOT is
 
 begin
-	process(a) is
+    process(a) is
     begin
-		q <= not a;
+        q <= a nand a;
     end process;
 
 end architecture;
