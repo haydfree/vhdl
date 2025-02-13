56a14cff468beb5b43ec6d2732157cf7b3fae9aa
Hayden Freeman <45716506+haydfree@users.noreply.github.com>
2025-02-13 16:03:13 -0600

    Create chip_mulbit_mux.vhd

---
+++ chip_mulbit_mux.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity CHIP_MULBIT_MUX is
+
+    port(
+        a: in std_logic_vector(15 downto 0);
+        b: in std_logic_vector(15 downto 0);
+        s: in std_logic_vector(15 downto 0);
+        q: out std_logic_vector(15 downto 0);
+    );
+
+end entity;
+
+architecture rlt of CHIP_MULBIT_MUX is
+
+begin
+    process(a,b,s) is
+    begin
+        q <= (a nand not s) nand (b nand s);
+    end process;
+
+end architecture; 
