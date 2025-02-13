7eaa14779e0007b664ed84cdbffa6e1770be14c4
Hayden Freeman <45716506+haydfree@users.noreply.github.com>
2025-02-13 12:55:25 -0600

    Update chip_and.vhdl

--- chip_and.vhdl
+++ chip_and.vhdl
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity CHIP_AND is
 
-	port(
+    port(
     	a: in std_logic;
         b: in std_logic;
         q: out std_logic;
@@ -11,12 +11,20 @@ entity CHIP_AND is
 
 end entity;
 
+-- Nand gate:
+-- | a | b | q |
+-- -------------
+-- | 0 | 0 | 1 |
+-- | 0 | 1 | 1 |
+-- | 1 | 0 | 1 |
+-- | 1 | 1 | 0 |
+
 architecture rlt of CHIP_AND is
 
 begin
-	process(a,b) is
+    process(a,b) is
     begin
-		q <= a and b;
+        q <= (a nand b) nand (a nand b);
     end process;
 
 end architecture;
