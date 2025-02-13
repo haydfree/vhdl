42eeef0aa4b2743c924d8a906f300a6cb6317c77
Hayden Freeman <45716506+haydfree@users.noreply.github.com>
2025-02-13 13:26:57 -0600

    Update chip_or.vhdl

--- chip_or.vhdl
+++ chip_or.vhdl
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity CHIP_OR is
 
-	port(
+    port(
     	a: in std_logic;
         b: in std_logic;
         q: out std_logic;
@@ -11,12 +11,28 @@ entity CHIP_OR is
 
 end entity;
 
+-- Nand gate:
+-- | a | b | q |
+-- -------------
+-- | 0 | 0 | 1 |
+-- | 0 | 1 | 1 |
+-- | 1 | 0 | 1 |
+-- | 1 | 1 | 0 |
+
+-- Or gate:
+-- | a | b | q |
+-- -------------
+-- | 0 | 0 | 0 |
+-- | 0 | 1 | 1 |
+-- | 1 | 0 | 1 |
+-- | 1 | 1 | 1 |
+
 architecture rlt of CHIP_OR is
 
 begin
-	process(a,b) is
+    process(a,b) is
     begin
-		q <= a or b;
+        q <= (a nand a) nand (b nand b);
     end process;
 
 end architecture;
