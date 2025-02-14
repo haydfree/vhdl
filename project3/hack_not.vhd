faff83555c8f819d063e7f5dd6cc05fc77c05d86
Hayden Freeman <haydenfreeman@yahoo.com>
2025-02-14 11:23:31 -0600

    ch3 restarting

---
+++ project3/hack_not.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity HACK_NOT is
+
+end entity;
+
+architecture rtl of HACK_NOT is
+
+begin
+
+end architecture;
---
+++ project3/test_hack_not.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity tb is
+end entity;
+
+architecture test of tb is
+
+begin
+
+end architecture;
